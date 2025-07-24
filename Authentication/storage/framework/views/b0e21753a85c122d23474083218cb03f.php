5cbc0671ffad6cba3ba18f266dee235c16c7885f
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-07-24 10:48:18 +0600

    add

---
+++ Authentication/storage/framework/views/b0e21753a85c122d23474083218cb03f.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Login</title>
+</head>
+<body>
+    
+    <div class="mx-auto mt-5 w-full max-w-xl bg-gray-200 rounded-lg shadow-lg p-6">
+        <div>
+            <h3 class="text-white text-lg font-semibold bg-blue-500 px-4 py-2 rounded-t-lg">Login</h3>
+        </div>
+        <div class="mt-4">
+            <form action="<?php echo e(route('loginMatch')); ?>" method="POST">
+                <?php echo csrf_field(); ?>
+            <div class="mt-3">
+                <label for="useremail" class="block text-sm font-medium text-gray-700">Email Address</label>
+                <input type="email" name="email" id="useremail"
+                    class="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500">
+            </div>
+            <div class="mt-3">
+                <label for="userpassword" class="block text-sm font-medium text-gray-700">Password</label>
+                <input type="password" name="password" id="userpassword"
+                    class="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500">
+            </div> 
+            <div class="mt-5 flex flex-col sm:flex-row gap-3">
+                <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition"> Login </button>
+                <a href="/" class="bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition text-center">Back</a>
+            </div>
+            </form>
+        </div>
+    </div>
+
+        <?php if($errors->any()): ?>
+            <div class="card-footer text-body-secondary">
+                <div class="alert alert-danger">
+                    <ul>
+                        <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <li><?php echo e($error); ?></li>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+
+</body>
+</html><?php /**PATH C:\Users\user\Desktop\Laravel_Project\Authentication\resources\views/login.blade.php ENDPATH**/ ?>
